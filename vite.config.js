b7bf02cdc97a614694b26e394ad864688d2c0d5c
AndrewFesenko <andrewfesenko10@gmail.com>
2025-06-09 11:21:01 -0400

    fixed deployment config and paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
-    base: "/"
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    base: "/",
 })
-
