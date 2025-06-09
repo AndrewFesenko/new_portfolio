5ff87165287b26e95bdea8e288cd0d8472ae590e
AndrewFesenko <andrewfesenko10@gmail.com>
2025-06-09 11:37:53 -0400

    Fix model paths with leading slashes

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,16 +18,16 @@ const navLinks = [
 ];
 
 const words = [
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
-    { text: "Systems", imgPath: "/images/hardware.svg" },
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
-    { text: "Systems", imgPath: "/images/hardware.svg" }
+    { text: "Ideas", imgPath: "images/ideas.svg" },
+    { text: "Concepts", imgPath: "images/concepts.svg" },
+    { text: "Designs", imgPath: "images/designs.svg" },
+    { text: "Code", imgPath: "images/code.svg" },
+    { text: "Systems", imgPath: "images/hardware.svg" },
+    { text: "Ideas", imgPath: "images/ideas.svg" },
+    { text: "Concepts", imgPath: "images/concepts.svg" },
+    { text: "Designs", imgPath: "images/designs.svg" },
+    { text: "Code", imgPath: "images/code.svg" },
+    { text: "Systems", imgPath: "images/hardware.svg" }
 ];
 
 const counterItems = [
@@ -39,53 +39,53 @@ const counterItems = [
 
 const logoIconsList = [
     {
-        imgPath: "/images/logos/company-logo-1.png",
+        imgPath: "images/logos/company-logo-1.png",
     },
     {
-        imgPath: "/images/logos/company-logo-2.png",
+        imgPath: "images/logos/company-logo-2.png",
     },
     {
-        imgPath: "/images/logos/company-logo-3.png",
+        imgPath: "images/logos/company-logo-3.png",
     },
     {
-        imgPath: "/images/logos/company-logo-4.png",
+        imgPath: "images/logos/company-logo-4.png",
     },
     {
-        imgPath: "/images/logos/company-logo-5.png",
+        imgPath: "images/logos/company-logo-5.png",
     },
     {
-        imgPath: "/images/logos/company-logo-6.png",
+        imgPath: "images/logos/company-logo-6.png",
     },
     {
-        imgPath: "/images/logos/company-logo-7.png",
+        imgPath: "images/logos/company-logo-7.png",
     },
     {
-        imgPath: "/images/logos/company-logo-8.png",
+        imgPath: "images/logos/company-logo-8.png",
     },
     {
-        imgPath: "/images/logos/company-logo-9.png",
+        imgPath: "images/logos/company-logo-9.png",
     },
     {
-        imgPath: "/images/logos/company-logo-10.png",
+        imgPath: "images/logos/company-logo-10.png",
     },
     {
-        imgPath: "/images/logos/company-logo-11.png",
+        imgPath: "images/logos/company-logo-11.png",
     },
 ];
 
 const abilities = [
     {
-        imgPath: "/images/seo.png",
+        imgPath: "images/seo.png",
         title: "Build → Break → Learn",
         desc: "Most of what I’ve learned came from trying, breaking things, and figuring out why they didn’t work. It sticks better that way.",
     },
     {
-        imgPath: "/images/chat.png",
+        imgPath: "images/chat.png",
         title: "Communication Matters",
         desc: "Clear, honest communication makes everything smoother. I always want the people I work with to know where things stand.",
     },
     {
-        imgPath: "/images/time.png",
+        imgPath: "images/time.png",
         title: "Questions, Always",
         desc: "I ask a lot of questions. Not to be annoying, it’s just how I learn and how I build better things.",
     },
@@ -95,23 +95,23 @@ const abilities = [
 const techStackImgs = [
     {
         name: "React Developer",
-        imgPath: "/images/logos/react.png",
+        imgPath: "images/logos/react.png",
     },
     {
         name: "Python Developer",
-        imgPath: "/images/logos/python.svg",
+        imgPath: "images/logos/python.svg",
     },
     {
         name: "Backend Developer",
-        imgPath: "/images/logos/node.png",
+        imgPath: "images/logos/node.png",
     },
     {
         name: "Interactive Developer",
-        imgPath: "/images/logos/three.png",
+        imgPath: "images/logos/three.png",
     },
     {
         name: "Project Manager",
-        imgPath: "/images/logos/git.svg",
+        imgPath: "images/logos/git.svg",
     },
 ];
 
@@ -151,13 +151,13 @@ const techStackIcons = [
 const expCards = [
     {
         tools: [
-            { name: "Unity", icon: "/images/unity.svg" },
-            { name: "C#", icon: "/images/csharp.png" },
-            { name: "Blender", icon: "/images/blender.png" },
-            { name: "Python", icon: "/images/python.svg" },
+            { name: "Unity", icon: "images/unity.svg" },
+            { name: "C#", icon: "images/csharp.png" },
+            { name: "Blender", icon: "images/blender.png" },
+            { name: "Python", icon: "images/python.svg" },
         ],
-        imgPath: "/images/uf_banner.png",
-        logoPath: "/images/ufcirc.png",
+        imgPath: "images/uf_banner.png",
+        logoPath: "images/ufcirc.png",
         title: "Undergraduate Researcher — GLaDE Lab (UF)",
         date: "Feb 2025 – Present",
         responsibilities: [
@@ -169,13 +169,13 @@ const expCards = [
     },
     {
         tools: [
-            { name: "C#", icon: "/images/csharp.png" },
-            { name: "JavaScript", icon: "/images/javascript.png" },
-            { name: "SQL", icon: "/assets/sql.svg" },
-            { name: "Azure", icon: "/assets/azure.svg" },
+            { name: "C#", icon: "images/csharp.png" },
+            { name: "JavaScript", icon: "images/javascript.png" },
+            { name: "SQL", icon: "assets/sql.svg" },
+            { name: "Azure", icon: "assets/azure.svg" },
         ],
-        imgPath: "/images/pbsc_site.png",
-        logoPath: "/images/pbsccirc.png",
+        imgPath: "images/pbsc_site.png",
+        logoPath: "images/pbsccirc.png",
         title: "Web Development Intern — Palm Beach State College",
         date: "June 2024 – Present",
         responsibilities: [
@@ -188,12 +188,12 @@ const expCards = [
     },
     {
         tools: [
-            { name: "AutoCAD", icon: "/assets/autocad.png" },
-            { name: "Manufacturing", icon: "/assets/machine.png" },
-            { name: "Blueprints", icon: "/assets/blueprint.png" },
+            { name: "AutoCAD", icon: "assets/autocad.png" },
+            { name: "Manufacturing", icon: "assets/machine.png" },
+            { name: "Blueprints", icon: "assets/blueprint.png" },
         ],
-        imgPath: "/images/instatech.png",
-        logoPath: "/images/instacirc.png",
+        imgPath: "images/instatech.png",
+        logoPath: "images/instacirc.png",
         title: "Assistant — Instatech Industries",
         date: "Oct 2021 – Aug 2023",
         responsibilities: [
@@ -204,11 +204,11 @@ const expCards = [
     },
     {
         tools: [
-            { name: "Service", icon: "/assets/customer-review.png" },
-            { name: "Teamwork", icon: "/assets/united.png" },
+            { name: "Service", icon: "assets/customer-review.png" },
+            { name: "Teamwork", icon: "assets/united.png" },
         ],
-        imgPath: "/images/lindburgers_banner.png",
-        logoPath: "/images/lindburgers.png",
+        imgPath: "images/lindburgers_banner.png",
+        logoPath: "images/lindburgers.png",
         title: "Server / Food Runner — Lindburgers",
         date: "May 2021 – Aug 2023",
         responsibilities: [
@@ -223,15 +223,15 @@ const expCards = [
 const expLogos = [
     {
         name: "logo1",
-        imgPath: "/images/logo1.png",
+        imgPath: "images/logo1.png",
     },
     {
         name: "logo2",
-        imgPath: "/images/logo2.png",
+        imgPath: "images/logo2.png",
     },
     {
         name: "logo3",
-        imgPath: "/images/logo3.png",
+        imgPath: "images/logo3.png",
     },
 ];
 
@@ -240,12 +240,12 @@ const socialImgs = [
     {
         name: "github",
         url: "https://github.com/AndrewFesenko",
-        imgPath: "/images/github.png",
+        imgPath: "images/github.png",
     },
     {
         name: "linkedin",
         url: "https://www.linkedin.com/in/andrew-fesenko-007bb3248\n",
-        imgPath: "/images/linkedin.png",
+        imgPath: "images/linkedin.png",
     },
 ];
 
@@ -269,19 +269,19 @@ export const myProjects = [
         subdesc:
             'EchoHire enables users to practice interviews by speaking with an AI agent, receive structured feedback, and review past performance. It supports role-specific technical and behavioral questions, offers Gemini-powered feedback summaries, and features a responsive dashboard to track progress over time.',
         href: 'https://github.com/AndrewFesenko/ai_mock_interview',
-        texture: '/textures/project/echohire.mp4',
-        logo: '/assets/ai-avatar.png',
+        texture: 'textures/project/echohire.mp4',
+        logo: 'assets/ai-avatar.png',
         logoStyle: {
             backgroundColor: '#0e1a1f', // darker and deeper than base card backgrounds
             border: '1px solid #1e2b31', // soft contrast, echoes your panel borders
             boxShadow: '0 0 30px rgba(178, 245, 176, 0.25)' // matches the mint glow from buttons and CTAs
         },
-        spotlight: '/assets/spotlight2.png',
+        spotlight: 'assets/spotlight2.png',
         tags: [
             {
                 id: 1,
                 name: 'React.js',
-                path: '/assets/react.svg',
+                path: 'assets/react.svg',
             },
             {
                 id: 4,
@@ -291,12 +291,12 @@ export const myProjects = [
             {
                 id: 3,
                 name: 'TypeScript',
-                path: '/assets/typescript.png',
+                path: 'assets/typescript.png',
             },
             {
                 id: 2,
                 name: 'Firebase ',
-                path: '/assets/firebase.png',
+                path: 'assets/firebase.png',
             },
         ],
     },
@@ -306,19 +306,19 @@ export const myProjects = [
         subdesc:
             'By leveraging machine learning, natural language processing, and chatbot design, the system identifies and visualizes complex character networks, detects evolving themes across episodes, and simulates realistic conversations with the main character Asta. This project combines data scraping, neural networks, and custom LLMs to provide deep, interactive insights into storylines and character dynamics.',
         href: 'https://github.com/AndrewFesenko/black_clover_nlp',
-        texture: '/textures/project/ShowAnalysis.mp4',
+        texture: 'textures/project/ShowAnalysis.mp4',
         logo: 'assets/blackclover.png',
         logoStyle: {
             backgroundColor: '#2A1816',
             border: '0.2px solid #36201D',
             boxShadow: '0px 0px 60px 0pxrgba(59, 33, 31, 0.3)',
         },
-        spotlight: '/assets/spotlight1.png',
+        spotlight: 'assets/spotlight1.png',
         tags: [
             {
                 id: 1,
                 name: 'Python',
-                path: '/assets/python.svg',
+                path: 'assets/python.svg',
             },
             {
                 id: 2,
@@ -343,8 +343,8 @@ export const myProjects = [
         subdesc:
             'The system uses PID control tuned via Bluetooth, enabling dynamic calibration and fine-tuning without physical access. Real-time sensor feedback is used to drive brushless motors, maintaining stability even during external disturbances. The entire cube was designed to be 3D printed, showcasing a full-stack embedded system from hardware to firmware.',
         href: 'https://github.com/AndrewFesenko/self-balancing-cube',
-        texture: '/textures/project/coming_soon.mp4',
-        logo: '/assets/project-logo3.png',
+        texture: 'textures/project/coming_soon.mp4',
+        logo: 'assets/project-logo3.png',
         logoStyle: {
             backgroundColor: '#60f5a1',
             background:
@@ -352,12 +352,12 @@ export const myProjects = [
             border: '0.2px solid rgba(208, 213, 221, 1)',
             boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
         },
-        spotlight: '/assets/spotlight3.png',
+        spotlight: 'assets/spotlight3.png',
         tags: [
             {
                 id: 1,
                 name: 'C++',
-                path: '/assets/C++.png',
+                path: 'assets/C++.png',
             },
             {
                 id: 2,
@@ -372,18 +372,18 @@ export const myProjects = [
         subdesc:
             'The system detects players and ball in live footage, assigns team colors using zero-shot classification, and maps real-world positions to a top-down court. Stats like ball possession, passes, and player movement are automatically tracked. Built in Python using OpenCV, NumPy, and Hugging Face APIs.',
         href: '',
-        texture: '/textures/project/coming_soon.mp4',
-        logo: '/assets/basketball_logo.png',
+        texture: 'textures/project/coming_soon.mp4',
+        logo: 'assets/basketball_logo.png',
         logoStyle: {
             backgroundColor: '#0e1a1f',
             border: '1px solid #1e2b31',
             boxShadow: '0 0 30px rgba(255, 144, 56, 0.25)'
         },
-        spotlight: '/assets/spotlight4.png',
+        spotlight: 'assets/spotlight4.png',
         tags: [
-            { id: 1, name: 'Python', path: '/assets/python.svg' },
-            { id: 2, name: 'OpenCV', path: '/assets/OpenCV.svg' },
-            { id: 3, name: 'Hugging Face', path: '/assets/huggingface.png' }
+            { id: 1, name: 'Python', path: 'assets/python.svg' },
+            { id: 2, name: 'OpenCV', path: 'assets/OpenCV.svg' },
+            { id: 3, name: 'Hugging Face', path: 'assets/huggingface.png' }
         ],
     },
     {
@@ -392,20 +392,20 @@ export const myProjects = [
         subdesc:
             'By integrating dynamic models and parametric solutions, students can adjust problem variables and immediately see how forces, reactions, and internal stresses change—making abstract concepts tangible and interactive.',
         href: '',
-        texture: '/textures/project/coming_soon.mp4',
-        logo: '/assets/UF.png',
+        texture: 'textures/project/coming_soon.mp4',
+        logo: 'assets/UF.png',
         logoStyle: {
             backgroundColor: '#1C1C21',
             border: '0.2px solid #2A2A2A',
             boxShadow: '0px 0px 60px 0px #4A90E24D',
         },
-        spotlight: '/assets/spotlight5.png',
+        spotlight: 'assets/spotlight5.png',
         status: 'upcoming',
         tags: [
             {
                 id: 1,
                 name: 'Unity',
-                path: '/assets/unity.svg',
+                path: 'assets/unity.svg',
             },
             {
                 id: 2,
@@ -415,12 +415,12 @@ export const myProjects = [
             {
                 id: 3,
                 name: 'Python',
-                path: '/assets/python.svg',
+                path: 'assets/python.svg',
             },
             {
                 id: 4,
                 name: 'Onshape',
-                path: '/assets/onshape.png',
+                path: 'assets/onshape.png',
             },
         ],
     },
@@ -431,34 +431,34 @@ export const myProjects = [
         subdesc:
             'Includes custom tutor and student sign-up flows, subject-based lesson browsing, and detailed descriptions of tutoring services and academic resources. The backend will be built with Node.js to support features like session management, TutorCruncher API integration, and transportation coordination for in-person events.',
         href: '',
-        texture: '/textures/project/coming_soon.mp4',
-        logo: '/assets/GRMR.png',
+        texture: 'textures/project/coming_soon.mp4',
+        logo: 'assets/GRMR.png',
         logoStyle: {
             backgroundColor: '#f5f3ff',
             border: '1px solid #dcd5fa',
             boxShadow: '0 0 30px rgba(118, 103, 184, 0.25)',
         },
-        spotlight: '/assets/spotlight4.png',
+        spotlight: 'assets/spotlight4.png',
         tags: [
             {
                 id: 1,
                 name: 'Next.js',
-                path: '/assets/nextjs.svg',
+                path: 'assets/nextjs.svg',
             },
             {
                 id: 2,
                 name: 'TypeScript',
-                path: '/assets/typescript.png',
+                path: 'assets/typescript.png',
             },
             {
                 id: 3,
                 name: 'Tailwind CSS',
-                path: '/assets/tailwindcss.png',
+                path: 'assets/tailwindcss.png',
             },
             {
                 id: 4,
                 name: 'Node.js',
-                path: '/assets/nodejs.svg',
+                path: 'assets/nodejs.svg',
             },
         ],
     }
@@ -502,14 +502,14 @@ export const aboutMeTopics = [
         title: "Photos",
         subtitle: "More About Me",
         images: [
-            { src: "/images/photos/pc_build.jpg", caption: "Built this myself. Still proud." },
-            { src: "/images/photos/circuits.jpg", caption: "My first PCB project: designed, soldered, and tested as part of UF's Circuits 1 final." },
-            { src: "/images/photos/pcb.jpg", caption: "My first self-designed PCB: built for a personal project using the ESP32." },
-            { src: "/images/photos/weld.JPG", caption: "Welded for the first time nearly seven years ago with my dad." },
-            { src: "/images/photos/dad.JPG", caption: "Me and my dad. He’s the reason I like building things." },
-            { src: "/images/photos/dog.JPG", caption: "Project supervisor."},
-            { src: "/images/photos/concert.jpg", caption: "My first ever concert." },
-            { src: "/images/photos/clash_base.PNG", caption: "Years of progress and no regrets." },
+            { src: "images/photos/pc_build.jpg", caption: "Built this myself. Still proud." },
+            { src: "images/photos/circuits.jpg", caption: "My first PCB project: designed, soldered, and tested as part of UF's Circuits 1 final." },
+            { src: "images/photos/pcb.jpg", caption: "My first self-designed PCB: built for a personal project using the ESP32." },
+            { src: "images/photos/weld.JPG", caption: "Welded for the first time nearly seven years ago with my dad." },
+            { src: "images/photos/dad.JPG", caption: "Me and my dad. He’s the reason I like building things." },
+            { src: "images/photos/dog.JPG", caption: "Project supervisor."},
+            { src: "images/photos/concert.jpg", caption: "My first ever concert." },
+            { src: "images/photos/clash_base.PNG", caption: "Years of progress and no regrets." },
         ],
     }
 ];
